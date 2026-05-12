142950a1233d97f03ff6a592f77d0bb028770616
RayRay-cloud <bentalebrayan46@gmail.com>
2026-05-12 11:54:16 +0100

    expression générale des matrices

--- notebook-day-2.py
+++ notebook-day-2.py
@@ -1262,6 +1262,19 @@ def _(mo):
     $A$ captures how the **current state** $\mathbf{x}$ influences $\dot{\mathbf{x}}$. It has **no input terms** (those go in $B$).
 
     $$
+    A =
+    \begin{bmatrix}
+    0 & 1 & 0 & 0 & 0  & 0 \\
+    0 & 0 & 0 & 0 & -g & 0 \\
+    0 & 0 & 0 & 1 & 0  & 0 \\
+    0 & 0 & 0 & 0 & 0  & 0 \\
+    0 & 0 & 0 & 0 & 0  & 1 \\
+    0 & 0 & 0 & 0 & 0  & 0
+    \end{bmatrix}
+    $$
+
+
+    $$
     A = \begin{bmatrix}
     0 & 1 & 0 & 0 & 0 & 0 \\
     0 & 0 & 0 & 0 & -1 & 0 \\
@@ -1289,7 +1302,17 @@ def _(mo):
     ### Matrix B
 
     $B$ captures how the **inputs** $\mathbf{u} = (\Delta f,\ \Delta\varphi)^T$ influence $\dot{\mathbf{x}}$.
-
+    $$
+    B =
+    \begin{bmatrix}
+    0 & 0\\
+    0 & -g\\
+    0 & 0\\
+    1/M & 0\\
+    0 & 0 \\
+    0 & -M g \ell/J\\
+    \end{bmatrix}
+    $$
     $$
     B = \begin{bmatrix}
     0 & 0 \\
